e314ad3eaafacb8c0f7235514016314e24d69a0f
Kirin <rudenokaleksandr21@gmail.com>
2025-06-12 17:10:08 +0300

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
+// Package main implements the entry point for the secure command-line password manager.
+// This file contains the implementation of the main function which is used to execute the application.
 package main
 
 import "github.com/kirinyoku/vlxck/cmd"
