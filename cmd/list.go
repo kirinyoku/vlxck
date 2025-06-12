cc69c6aee1b11be79d94af57e34ffd424f64fda1
Kirin <rudenokaleksandr21@gmail.com>
2025-06-12 17:03:49 +0300

    add command for listing secrets

--- cmd/list.go
+++ cmd/list.go
@@ -1,40 +1,45 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
+// Package cmd implements the command-line interface for the secure secret manager.
+// This file contains the implementation of the 'list' command which is used to
+// list all secrets in the encrypted store.
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/kirinyoku/vlxck/internal/store"
+	"github.com/kirinyoku/vlxck/internal/utils"
 	"github.com/spf13/cobra"
 )
 
-// listCmd represents the list command
+// listCmd represents the 'list' command that allows users to list all secrets in the store.
+// It prompts the user for the master password and displays the list of secrets.
+// If the store is not found, it displays a message indicating that the store was not found.
+//
+// The command requires the following flags:
+//   - category (-c): Optional category for filtering secrets
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List all secrets",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		filePath := getStorePath()
+		password := utils.PromptForPassword("Enter master password: ")
+		s, err := store.LoadStore(filePath, password)
+		if err != nil {
+			fmt.Println("Error loading store:", err)
+			return
+		}
+		category, _ := cmd.Flags().GetString("category")
+		for _, secret := range s.Secrets {
+			if category == "" || secret.Category == category {
+				fmt.Printf("Name: %s, Category: %s\n", secret.Name, secret.Category)
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Define command flags with shorthand and descriptions
+	listCmd.Flags().StringP("category", "c", "", "Filter by category")
 }
